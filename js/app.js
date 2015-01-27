8c8ccda49476c3db40fa82f3e5ddf297b331d08f
Grace Lee <gralee10@gmail.com>
2015-01-26 21:25:01 -0800

    Fixed currency display and total / remaining balance. Added font family.

--- js/app.js
+++ js/app.js
@@ -14,19 +14,23 @@ var start = 100.00;
 var totalspent;
 
 
-localStorage.setItem("stringy", null);
-totalspent=0
+//localStorage.setItem("stringy", null);
+
 
 function renderHomeView() {
 	$('body').html(template(context));
 	$("#mainBtn").on('click', function() {
 		i1 = $("#item1").val(); 
 		i2 = $("#item2").val(); 
-		i3 = parseInt($("#item3").val());
+		i3 = (parseFloat($("#item3").val())).toLocaleString("en-GB",{style: "currency", currency: "USD", minimumFractionDigits:2});
 		if (JSON.parse(localStorage.getItem("stringy")) === null) {
-			i4 =  context.items[0].remainder - i3;
+			i4 =  (parseFloat(context.items[0].remainder.substring(1)) - parseFloat(i3.substring(1))).toLocaleString(
+				"en-GB",{style: "currency", currency: "USD", minimumFractionDigits:2}
+				);
 		} else {
-			i4 = JSON.parse(localStorage.getItem("stringy")).items[JSON.parse(localStorage.getItem("stringy")).items.length-1].remainder - i3;
+			i4 = (parseFloat(JSON.parse(localStorage.getItem("stringy")).items[JSON.parse(localStorage.getItem("stringy")).items.length-1].remainder.substring(1)) - parseFloat(i3.substring(1))).toLocaleString(
+				"en-GB",{style: "currency", currency: "USD", minimumFractionDigits:2}
+				);
 		}
 		
 		newItem = {
@@ -37,17 +41,17 @@ function renderHomeView() {
 		};
 		context.items.push(newItem);
 
+		totalspent=0
 
-
-		for (i=0; i < context.items.length; i++) {		
-			totalspent += parseInt(context.items[i].price);
-			alert(parseInt(context.items[i].price));
+		for (i=0; i < context.items.length; i++) {
+			totalspent += parseFloat(context.items[i].price.substring(1));
+			//alert(parseFloat(context.items[i].price));
 		};
-		context.total = totalspent; 
-		context.balance = context.items[0].remainder - totalspent;
-		alert(context.items[0].remainder);
-		alert(totalspent);
-		alert(context.balance)
+		context.total = (totalspent).toLocaleString("en-GB",{style: "currency", currency: "USD", minimumFractionDigits:2}); 
+		context.balance = (parseFloat(context.items[0].remainder.substring(1)) - totalspent).toLocaleString("en-GB",{style: "currency", currency: "USD", minimumFractionDigits:2});
+		//alert(context.items[0].remainder);
+		//alert(totalspent);
+		//alert(context.balance)
 
 		stringy = JSON.stringify(context);
 		localStorage.setItem("stringy", stringy);
@@ -62,8 +66,8 @@ if (JSON.parse(localStorage.getItem("stringy")) === null) {
 		items: [{
 			name: "Starting Balance",
 			details: "We start here.",
-			price: 0,
-			remainder: 100
+			price: (0).toLocaleString("en-GB",{style: "currency", currency: "USD", minimumFractionDigits:2}),
+			remainder: (100).toLocaleString("en-GB",{style: "currency", currency: "USD", minimumFractionDigits:2})
 		}],
 		total: "0.00",
 		balance: "Your remaining amount"
